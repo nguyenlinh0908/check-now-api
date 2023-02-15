61bdae8ef050b60558b8a20317e8baf3c4cab937
linhnv-comartek <linhnv@comartek.com>
2023-02-15 15:14:12 +0700

    update user module

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,13 +1,29 @@
 import { Role } from 'src/modules/auth/enums';
 import { ApiProperty } from '@nestjs/swagger';
+import { GenderEnum } from '../enums';
 
 export class CreateUserDto {
-  @ApiProperty({})
+  @ApiProperty({ type: String, default: 'username' })
   username: string;
 
-  @ApiProperty({})
+  @ApiProperty({ type: String, default: 'password' })
   password: string;
 
-  @ApiProperty({})
+  @ApiProperty({ type: String, default: 'Nguyễn Văn A' })
+  fullName: string;
+
+  @ApiProperty({ type: String, default: 'Định Công - Hoàng Mai - Hà Nội' })
+  address: string;
+
+  @ApiProperty({ type: Number, default: 1676448433000 })
+  dateOfBirth: number;
+
+  @ApiProperty({ type: String, default: GenderEnum.MALE })
+  gender: GenderEnum;
+
+  @ApiProperty({ type: String, default: '0366537938' })
+  phone: string;
+
+  @ApiProperty({ type: String, default: Role.USER })
   role: Role;
 }
--- src/modules/user/models/user.model.ts
+++ src/modules/user/models/user.model.ts
@@ -1,5 +1,7 @@
+import { dateToTimestamp, timeStampToDate } from 'src/helpers';
 import { Role } from 'src/modules/auth/enums';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { GenderEnum } from '../enums';
 
 @Entity()
 export class User {
@@ -17,6 +19,43 @@ export class User {
     // select: false,
   })
   password: string;
+
+  @Column({
+    type: String,
+  })
+  fullName: string;
+
+  @Column({
+    type: String,
+  })
+  address: string;
+
+  @Column({
+    type: Date,
+    transformer: {
+      to(value) {
+        // before insert
+        return timeStampToDate(value);
+      },
+      from(value) {
+        // after query
+        return dateToTimestamp(value);
+      },
+    },
+    default: '1970-01-01 00:00:00',
+  })
+  dateOfBirth: Date;
+
+  @Column({
+    type: 'enum',
+    enum: GenderEnum,
+  })
+  gender: GenderEnum;
+
+  @Column({
+    type: String,
+  })
+  phone: string;
 
   @Column({
     type: 'enum',
