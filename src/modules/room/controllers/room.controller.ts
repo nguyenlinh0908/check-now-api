a42f720085d2f06fda407420c7b5b5fcafd6827f
linhnv-comartek <linhnv@comartek.com>
2023-04-18 23:56:49 +0700

    room detail

--- src/modules/room/controllers/room.controller.ts
+++ src/modules/room/controllers/room.controller.ts
@@ -1,5 +1,13 @@
 import { FilterRoomDto } from './../dto';
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { format } from 'date-fns';
 import {
   ApiBearerAuth,
@@ -14,7 +22,7 @@ import { ICurrentUser } from 'src/modules/auth/interfaces';
 import { CreateRoomDto } from '../dto';
 import { RoomService } from '../services';
 import { pick } from 'lodash';
-import { Room } from '../models'
+import { Room } from '../models';
 
 @ApiTags('Room')
 @Controller('room')
@@ -34,7 +42,7 @@ export class RoomController {
       body.user = Number(user.id);
     }
     const room = await this.roomService.create(body);
-    
+
     return {
       id: room.id,
     };
@@ -45,8 +53,8 @@ export class RoomController {
   async rooms(@Query() filterRoom: FilterRoomDto) {
     const filter = pick(filterRoom, ['province', 'district', 'ward', 'type']);
     const order = pick(filterRoom, ['order_by']);
-    const rooms = await this.roomService.find( filter, order);
-    
+    const rooms = await this.roomService.find(filter, order);
+
     const formattedRooms = rooms.map((room) => {
       return {
         id: room.id,
@@ -61,16 +69,16 @@ export class RoomController {
         avatar: room['avatar'],
         favorite: room['favorite'],
         address: `${room.ward['_name']}, ${room.district['_name']}, ${room.province['_name']}`,
-        created_at: format(room.created_at, "dd-MM-yyyy HH:mm")
-      }
-    })
+        created_at: format(room.created_at, 'dd-MM-yyyy HH:mm'),
+      };
+    });
     return formattedRooms;
   }
 
   @ApiCreatedResponse({ description: 'room detail' })
   @Get(':id')
   async room(
-    @Query('id')
+    @Param('id')
     id: String,
   ) {
     return await this.roomService.findOne(id);
@@ -96,11 +104,5 @@ export class RoomController {
   @Get('list/all')
   async allRooms() {
     return await this.roomService.findAll();
-  }
-
-  @ApiOkResponse({ description: 'get detail room' })
-  @Get('detail')
-  async detail(@Query('id') id: number): Promise<Room> {
-    return await this.roomService.findById(id);
   }
 }
